9d8e3c5f6cfbfa5d3275f7b3bf7dbe53120697a4
PavelDrg <pavel.dragan2@gmail.com>
2024-05-12 16:14:09 +0300

    comentarii in romana

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Import necessary libraries
 import numpy as np
 import mediapipe as mp
 from mediapipe.tasks import python
@@ -8,17 +7,47 @@ import os
 import pyttsx3
 from moviepy.editor import VideoFileClip, AudioFileClip
 
-# Constants
-MARGIN = 10  # pixels
-ROW_SIZE = 10  # pixels
+from tensorflow import keras
+import  keras_cv
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# --------------------------------------------------------
+# GENERAREA DE IMAGINI
+
+keras.mixed_precision.set_global_policy("mixed_float16")
+
+# Crearea modelului
+model = keras_cv.models.StableDiffusion(img_height=512,
+                                        img_width=512,
+                                        jit_compile=True)
+
+def plot_images(images):
+    plt.figure(figsize=(20, 20))
+    for i in range(len(images)):
+        ax = plt.subplot(1, len(images), i + 1)
+        plt.imshow(images[i])
+        plt.axis("off")
+
+# Aici dam prompt-ul in functie de asignare
+images = model.text_to_image(prompt="Realistic beach scene with a black surfboard as the main subject and the light blue sea in the background.",
+                             batch_size=1)
+
+plot_images(images)
+
+# --------------------------------------------------------
+# PRELUCRAREA IMAGINII ALESE
+
+# Constante pentru partea de detectie de obiect (modificam in caz ca vrem sa arate altfel incercuirea obiectului)
+MARGIN = 10  # pixeli
+ROW_SIZE = 10  # pixeli
 FONT_SIZE = 1
 FONT_THICKNESS = 1
-TEXT_COLOR = (0, 255, 0)  # green
+TEXT_COLOR = (0, 255, 0)
 
 
-# Function to visualize bounding boxes on the input image
+# Vizualizam bounding box pe imaginea originala
 def visualize(image, detection_result):
-    """Draws bounding boxes on the input image and return it."""
     for detection in detection_result.detections:
         bbox = detection.bounding_box
         start_point = bbox.origin_x, bbox.origin_y
@@ -27,52 +56,52 @@ def visualize(image, detection_result):
     return image
 
 
-# Function to create a binary mask using GrabCut
+# Functie de creare a mastii binare cerute folosind GrabCut
 def grabcut_segmentation(image, rect, iter_count=10):
-    # Initialize mask
+    # Initializam masca
     mask = np.zeros(image.shape[:2], dtype="uint8")
-    # Initialize model arrays
+
     fg_model = np.zeros((1, 65), dtype="float")
     bg_model = np.zeros((1, 65), dtype="float")
-    # Apply GrabCut
+    # Aplicam GrabCut
     (mask, bg_model, fg_model) = cv2.grabCut(image, mask, rect, bg_model, fg_model,
                                              iterCount=iter_count, mode=cv2.GC_INIT_WITH_RECT)
-    # Generate binary mask
+    # Generam masca binara
     output_mask = np.where((mask == cv2.GC_BGD) | (mask == cv2.GC_PR_BGD), 0, 1)
-    # Scale the mask
+    # Scalam masca
     output_mask = (output_mask * 255).astype("uint8")
-    # Apply bitwise AND to generate segmented object
+    # Aplicam un AND bit cu bit ca sa generam obiectul extras
     segmented_object = cv2.bitwise_and(image, image, mask=output_mask)
     return output_mask, segmented_object
 
 
-# Load input image
+# Citim imaginea originala
 IMAGE_FILE = 'image.png'
 img = cv2.imread(IMAGE_FILE)
 
-# Create ObjectDetector object
+# Cream modelul de detectare de obiecte
 base_options = python.BaseOptions(model_asset_path='efficientdet_lite0.tflite')
 options = vision.ObjectDetectorOptions(base_options=base_options, score_threshold=0.5)
 detector = vision.ObjectDetector.create_from_options(options)
 
-# Load the input image
+# Incarcam imaginea originala
 image = mp.Image.create_from_file(IMAGE_FILE)
 
-# Detect objects in the input image
+# Detectam obiectul
 detection_result = detector.detect(image)
 
-# Process the detection result
+# Procesam rezultatul detectiei
 image_copy = np.copy(image.numpy_view())
 annotated_image = visualize(image_copy, detection_result)
 rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
 
-# Extract bounding box coordinates from the first program
+# Luam coordonatele bounding box-ului
 for detection in detection_result.detections:
     bbox = detection.bounding_box
     rect = (int(bbox.origin_x), int(bbox.origin_y), int(bbox.width), int(bbox.height))
-    break  # Assuming there's only one object
+    break
 
-# Apply GrabCut using the bounding box segmentation method
+# Aplicam GrabCut folosind coordonatele de la detectie
 grabcut_mask, segmented_object = grabcut_segmentation(rgb_annotated_image, rect)
 
 output_folder = os.path.dirname(__file__)
@@ -81,13 +110,15 @@ cv2.imwrite(os.path.join(output_folder, 'object_detected_img.png'), rgb_annotate
 cv2.imwrite(os.path.join(output_folder, 'binary_mask.png'), grabcut_mask)
 cv2.imwrite(os.path.join(output_folder, 'extracted_object.png'), segmented_object)
 
-# Convert input image to YUV color space
+# Convertim imaginea originala in spatiul de culoare YUV
 yuv_img = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
 cv2.imwrite(os.path.join(output_folder, 'yuv_img.tiff'), yuv_img)
 
-# Convert segmented object to YUV color space
+# Convertim obiectul extras in spatiul de culoare YUV
 yuv_segmented_object = cv2.cvtColor(segmented_object, cv2.COLOR_BGR2YUV)
 cv2.imwrite(os.path.join(output_folder, 'yuv_extracted_object.tiff'), yuv_segmented_object)
+
+# In caz ca vrem sa fie afisate imaginile prelucrate nu doar salvate decomentam codul de mai jos
 
 # WINDOW_WIDTH = 800
 # WINDOW_HEIGHT = 800
@@ -105,12 +136,15 @@ cv2.imwrite(os.path.join(output_folder, 'yuv_extracted_object.tiff'), yuv_segmen
 
 print("Imagini prelucrate")
 
-# Constants
+# --------------------------------------------------------
+# CREAREA PRIMULUI VIDEOCLIP
+
+# Constante pentru videoclip
 fps = 25.0
 duration = 5  # seconds
 dimensions = (1900, 1900)
 
-# Define the list of image paths
+# Caile catre imaginile folosite
 image_paths = [
     'image.png',
     'object_detected_img.png',
@@ -120,100 +154,93 @@ image_paths = [
     'yuv_extracted_object.tiff'
 ]
 
-# Define custom durations for each image (in seconds)
-custom_durations = [3, 2.5, 2, 2, 5, 5]  # Modify these values as needed
+# Listam pentru cate secunde vrem sa apara fiecare imagine
+custom_durations = [3, 2.5, 2, 2, 5, 5]
 
-# Initialize video writer
+# Initializam video writer (introducem aici si codecul dorit)
 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+# fourcc = cv2.VideoWriter_fourcc(*'3IVD')
+# fourcc = cv2.VideoWriter_fourcc(*'DX50')
 
 output_video = cv2.VideoWriter('video.avi', fourcc, fps, dimensions)
 
-# Define the text parameters
+# Proprietatile textului din clip
 font = cv2.FONT_HERSHEY_SIMPLEX
-text_position = (100, 100)  # Position of the text in the frame
+text_position = (100, 100)
 font_scale = 2
-font_color = (255, 255, 255)  # White color
+font_color = (255, 255, 255)
 thickness = 2
 
-# Define the text to be displayed for each image
+# Textul corespunzator fiecarei imagini
 image_texts = ['Original image with black surfboard on the beach', 'Image with surfboard detected', 'Extracted object', 'Mask with the object', 'Original image in YUV color space saved as tiff file', 'Mask with object in YUV colorspace saved as tiff file']
 
-# OpenCV VideoWriter doesn't support transparent text overlay, so we'll create a black background
+# Facem un fundal pentru text
 black_frame = np.zeros((dimensions[1], dimensions[0], 3), dtype=np.uint8)
 
-# Write each image to the video with text overlay
+# Punem fiecare imagine in video impreuna cu textul
 for image_path, text, duration in zip(image_paths, image_texts, custom_durations):
-    # Read the image
     img = cv2.imread(image_path)
 
-    # Check if the image was successfully read
     if img is None:
         print(f"Error: Unable to read image from path: {image_path}")
         continue
 
-    # Resize the image to fit within the frame
-    img_resized = cv2.resize(img, (1560, 1560))  # Adjust dimensions as needed
+    img_resized = cv2.resize(img, (1560, 1560))
 
-    # Add text to the frame
     frame_with_text = black_frame.copy()
-    # Split the text into lines
-    lines = text.split('\n')
+    cv2.putText(frame_with_text, text, text_position, font, font_scale, font_color, thickness, cv2.LINE_AA)
 
-    # Draw each line of text
-    y_offset = text_position[1]
-    for line in lines:
-        cv2.putText(frame_with_text, line, (text_position[0], y_offset), font, font_scale, font_color, thickness,
-                    cv2.LINE_AA)
-        y_offset += int(18 * font_scale * thickness)
-
-    # Calculate the position to place the resized image
     x_offset = (frame_with_text.shape[1] - img_resized.shape[1]) // 2
     y_offset = (frame_with_text.shape[0] - img_resized.shape[0]) // 2
 
-    # Combine the image with the frame containing the text
     frame_with_text[y_offset:y_offset + img_resized.shape[0], x_offset:x_offset + img_resized.shape[1]] = img_resized
 
-    # Write the frame to the video with custom duration
     for _ in range(int(fps * duration)):
         output_video.write(frame_with_text)
 
-# Release the video writer
+# Finalizam clipul
 output_video.release()
 print("Video Saved")
 
-# Initialize the pyttsx3 engine
+# --------------------------------------------------------
+# CREAREA INREGISTRARII AUDIO
+
+# Pornim engine-ul pentru inregistrarea audio
 engine = pyttsx3.init()
 
-# Set properties (optional)
-engine.setProperty('rate', 150)  # Speed of speech
+# Proprietatile vocii folosite (am modificat doar rate of speech-ul)
+engine.setProperty('rate', 150)
 
 
-# Define the texts to be spoken
+# Introducem aici textul dorit
 text = ('Original image with black surfboard on the beach. Image with surfboard detected. Extracted object. Mask '
         'with the object. Original image in YUV color space saved as tiff file. Mask with object in YUV colorspace '
         'saved as tiff file.')
 
-# Define the output audio file name
+# Cum e salvata inregistrarea
 output_file = "audio.mp3"
 
-# Iterate through the texts
-engine.save_to_file(text, output_file)  # Save text to the output file
+# Salvam in fisier
+engine.save_to_file(text, output_file)
 
 engine.runAndWait()
 
 print("Audio inregistrat")
 
-# Load the video and audio files
+# --------------------------------------------------------
+# CREAREA VIDEOCLIPULUI CU SONOR
+
+# Incarcam fisierul audio si video
 video = VideoFileClip("video.avi")
 audio = AudioFileClip("audio.mp3")
 
-# Set the audio of the video to the audio from audio.mp3
+# Punem audioul in video
 video = video.set_audio(audio)
 
-# Write the modified video with the new audio using the DivX codec
+# Scriem fisierul video modificat (putem schimba codecul)
 video.write_videofile("video_with__audio.avi", codec="libxvid", audio_codec="mp3")
 
-# Optional: If you want to overwrite the old video with the new one
+# In caz ca vrem sa avem doar videoclipul cu sunet decomentam urmatoarea linie
 #os.replace("video_with_audio.avi", "video.avi")
 
 print("Clip final salvat")
